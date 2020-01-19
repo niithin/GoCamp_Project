38a0dfc497ed7b8c559d028e4c696087c99c9e31
nithin <nithinchandra1996@gmail.com>
2020-01-19 17:58:48 +0000

    add mongolab

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes=require("./routes/comments"),
 
 
 /*seedDB();*/
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser: true});
+mongoose.connect("mongodb+srv://nithindb:Amma*Nana@96@cluster0-c6ial.mongodb.net/test?retryWrites=true&w=majority",{useNewUrlParser: true,useCreateIndex:true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
