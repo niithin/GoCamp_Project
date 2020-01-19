7404575564e92e782a7cede5bfbec6d95f139c07
nithin <nithinchandra1996@gmail.com>
2020-01-19 17:13:21 +0000

    initial deploy commit

---
+++ routes/campground.js
@@ -0,0 +1,80 @@
+var express=require("express");
+var router=express.Router();
+var Campground=require("../models/campground");
+var middleware=require("../middleware");
+
+router.get("/",function(req,res){
+	
+	Campground.find({},function(err,allCampgrounds){
+		if(err){
+			console.log(err);
+		}else{
+		    res.render("index",{campgrounds:allCampgrounds,currentUser:req.user});	
+		}
+	});
+});
+router.post("/",middleware.isLoggedIn,function(req,res){
+	var name=req.body.name;
+	var price=req.body.price;
+	var image=req.body.image;
+	var desc=req.body.description;
+	var author={
+		id:req.user._id,
+		username:req.user.username
+	}
+	var newCampground={name:name,price:price,image:image,description:desc,author:author}
+	Campground.create(newCampground,function(err,newlyCreated){
+		if(err){
+			console.log(err);
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+router.get("/new",middleware.isLoggedIn,function(req,res){
+	res.render("new.ejs");
+});
+router.get("/:id",function(req,res){
+	Campground.findById(req.params.id).populate("comments").exec(function(err,foundCampground){
+		if(err){
+			console.log(err);
+		}else{
+			console.log(foundCampground);
+			res.render("shows",{campground:foundCampground});
+		}
+	});
+});
+
+router.get("/:id/edit",middleware.checkCampgroundOwnership,function(req,res){
+
+Campground.findById(req.params.id,function(err,foundCampground){
+res.render("edit",{campground:foundCampground});
+	});
+});
+
+router.put("/:id",middleware.checkCampgroundOwnership,function(req,res){
+	
+	Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.redirect("/campgrounds/" +req.params.id);
+		}
+	});
+});
+
+router.delete("/:id",middleware.checkCampgroundOwnership,function(req,res){
+	Campground.findByIdAndRemove(req.params.id,function(err){
+		if(err){
+			res.redirect("/campgrounds");
+		}else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+
+
+
+module.exports=router;
